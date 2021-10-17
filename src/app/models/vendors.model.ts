f67e8808567641c3e58b2f79f80d027a817d5313
YoannEddy <eddyyoann@gmail.com>
2021-10-17 13:08:12 +0000

    feat: add support for crypto vendor profile 2

---
+++ src/app/models/vendors.model.ts
@@ -0,0 +1,13 @@
+export interface Vendor {
+    created_at:string;
+    crypto_wallet: any;
+    crypto_wallet_type: string;
+    description: string;
+    id: string;
+    store_link: string;
+    store_name: string;
+    updated_at:string;
+    user: any;
+    user_id: string;
+    posts: any[];
+}
--- src/app/views/payements/payements.component.ts
+++ src/app/views/payements/payements.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Subject } from 'rxjs';
 import { map, switchMap, takeUntil } from 'rxjs/operators';
+import { Vendor } from 'src/app/models/vendors.model';
 import { VendorsService } from 'src/app/services/vendors.service';
 
 @Component({
@@ -37,6 +38,7 @@ export class PayementsComponent implements OnInit {
   username: string | null = '';
   vendorID: any;
   vendorData: any;
+  vendorPosts: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -55,9 +57,22 @@ export class PayementsComponent implements OnInit {
       .getVendorDetails(vendorID)
       .pipe(takeUntil(this.unsubscribeAll$))
       .subscribe((response: any) => {
-        this.vendorData = response['data'];
+        const vendor: Vendor = response['data'];
+        this.vendorData = vendor;
+        this.vendorPosts = vendor.posts;
         return this.vendorData
       });
+  }
+
+  getCryptoIcon(crypto_type: string) {
+    const baseRoute: string = 'assets\cripto\\';
+    switch(crypto_type) {
+      case 'Bitcoin': return `${baseRoute}1200px-BTC_Logo.svg.png`;
+      case 'Ethereum': return `${baseRoute}download (9).png`;
+      case 'USDT': return `${baseRoute}tether-usdt-logo.pn`;
+      case 'Bitcoin Cash': return `${baseRoute}bitcoin-cash-circle.png`;
+      default: return `${baseRoute}1200px-BTC_Logo.svg.png`;
+    }
   }
 
   getUrlParams(url: string) {
